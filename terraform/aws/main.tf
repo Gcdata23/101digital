eef92afa246a71d9a9c40382c824099aae3d9780
Your Name <you@example.com>
2024-11-11 21:26:30 +0700

    MaxWeather App

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -66,9 +66,9 @@ data "aws_eks_cluster_auth" "eks" {
 provider "helm" {
   registry_config_path = "repositories.yaml"
   kubernetes {
-#    host = data.aws_eks_cluster.eks.endpoint
-#    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-#    token                  = data.aws_eks_cluster_auth.eks.token
+    host = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
 
     config_path = "~/.kube/config"
   }
